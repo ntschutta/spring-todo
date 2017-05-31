312f5b07c9c85e6afdf46975bf6fb956d23725c2
Nate Schutta <ntschutta@gmail.com>
2017-05-31 10:26:33 -0500

    added Todo entity, lombok to avoid useless boilerplate

---
+++ src/main/java/io/sporing/Todo.java
@@ -0,0 +1,24 @@
+package io.sporing;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * Created by nschutta on 5/31/17.
+ */
+
+@Data
+@Entity
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String todo;
+    private boolean completed;
+
+}
