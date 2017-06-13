24672f768e4ccdc08e0829e093120e973d2a301f
Nate Schutta <ntschutta@gmail.com>
2017-06-13 08:23:27 -0500

    removed extraneous finder methods

--- src/main/java/io/sporing/TodoListRepository.java
+++ src/main/java/io/sporing/TodoListRepository.java
@@ -2,12 +2,10 @@ package io.sporing;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import java.util.List;
 
 /**
  * Created by nschutta on 6/2/17.
  */
 @RepositoryRestResource(collectionResourceRel = "todoList", path = "todoList")
 public interface TodoListRepository extends JpaRepository <TodoList, Long>  {
-    List<TodoList> findAll();
 }
--- src/main/java/io/sporing/TodoRepository.java
+++ src/main/java/io/sporing/TodoRepository.java
@@ -2,12 +2,10 @@ package io.sporing;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import java.util.List;
 
 /**
  * Created by nschutta on 5/31/17.
  */
 @RepositoryRestResource(collectionResourceRel = "todos", path = "todos")
 public interface TodoRepository extends JpaRepository <Todo, Long> {
-    List<Todo> findAll();
 }
