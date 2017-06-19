d4f8b9a6224302947047e6707324e2b0832b5147
Nate Schutta <ntschutta@gmail.com>
2017-06-19 10:21:56 -0500

    added helper finder for testing purposes

--- src/main/java/io/sporing/TodoRepository.java
+++ src/main/java/io/sporing/TodoRepository.java
@@ -8,4 +8,5 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
  */
 @RepositoryRestResource(collectionResourceRel = "todos", path = "todos")
 public interface TodoRepository extends JpaRepository <Todo, Long> {
+    Todo findByTodo(String todo);
 }
